b47c2254b1af4175d908fcb8b0962fc5f8d3ee26
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-11-30 16:34:47 +0100

    Update Brick.hx

--- source/stuff/Brick.hx
+++ source/stuff/Brick.hx
@@ -1,8 +1,9 @@
 package stuff;
 
-
-
 class Brick extends FlxSprite {
-
   var normalBricks:Array<String> = []; // this bricks dont do nothing special
+  var texture(default, set):String = '';
+  var brickType(default, set):String = '';
+
+  
 }
