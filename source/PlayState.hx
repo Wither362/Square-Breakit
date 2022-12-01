83e01c75bc6bf0e96a626e869bea15d0d663a345
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-01 20:32:08 +0100

    sysys

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,18 @@
 package;
 
 import flixel.FlxState;
+import flixel.group.FlxSpriteGroup;
+import stuff.Ball;
 
 class PlayState extends FlxState {
-  var initied:Bool = false;
-  var balls:FlxSpriteGroup<Balls>;
+	var initied:Bool = false;
+	var balls:FlxSpriteGroup<Balls>;
 
-  override function create() {
-    super.create();
-  }
+	override function create() {
+		super.create();
+	}
 
-  override function update(elapsed:Float) {
-    super.update(elapsed);
-  }
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+	}
 }
