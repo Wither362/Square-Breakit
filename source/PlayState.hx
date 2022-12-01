fb970dcdab1c089434590e52e45283d625b53179
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-01 11:27:31 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,4 +3,13 @@ package;
 import flixel.FlxState;
 
 class PlayState extends FlxState {
+  var initied:Bool = false;
+
+  override function create() {
+    super.create();
+  }
+
+  override function update(elapsed:Float) {
+    super.update(elapsed);
+  }
 }
