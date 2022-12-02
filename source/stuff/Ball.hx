09256892f4dee7dff4dd772341e8e037840703e4
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-12-02 14:29:28 -0500

    its loadGraphic not LoadGraphics CRAP

--- source/stuff/Ball.hx
+++ source/stuff/Ball.hx
@@ -11,25 +11,25 @@ class Ball extends FlxSprite {
     public function setType(v:String) {
         switch(v) {
             case 'basketball':
-                loadGraphics(Paths.image('balls/base/basketball' + '' + '$BALL')); //idk if it can have spaces
+                loadGraphic(Paths.image('balls/base/basketball' + '' + '$BALL')); //idk if it can have spaces
             case 'blueball':
-                loadGraphics(Paths.image('balls/base/blue' + '' + '$BALL'));
+                loadGraphic(Paths.image('balls/base/blue' + '' + '$BALL'));
             case 'cannonball':
-                loadGraphics(Paths.image('balls/base/cannon' + '' + '$BALL'));
+                loadGraphic(Paths.image('balls/base/cannon' + '' + '$BALL'));
             case 'magnetball':
-                loadGraphics(Paths.image('balls/base/magnet' + '' + '$BALL'));
+                loadGraphic(Paths.image('balls/base/magnet' + '' + '$BALL'));
             case 'miniorangeball':
-                loadGraphics(Paths.image('balls/base/mini' + '' + 'orange' + '' + '$BALL'));
+                loadGraphic(Paths.image('balls/base/mini' + '' + 'orange' + '' + '$BALL'));
             case 'orangeball':
-                loadGraphics(Paths.image('balls/base/orange' + '' + '$BALL'));
+                loadGraphic(Paths.image('balls/base/orange' + '' + '$BALL'));
             case 'plasmaball':
-                loadGraphics(Paths.image('balls/base/plasma' + '' + '$BALL'));
+                loadGraphic(Paths.image('balls/base/plasma' + '' + '$BALL'));
             case 'sniperball':
-                loadGraphics(Paths.image('balls/base/sniper' + '' + '$BALL'));
+                loadGraphic(Paths.image('balls/base/sniper' + '' + '$BALL'));
             case 'toxicball':
-                loadGraphics(Paths.image('balls/base/toxic' + '' + '$BALL'));
+                loadGraphic(Paths.image('balls/base/toxic' + '' + '$BALL'));
             case 'yellowball':
-                loadGraphics(Paths.image('balls/base/yellow' + '' + '$BALL'));
+                loadGraphic(Paths.image('balls/base/yellow' + '' + '$BALL'));
             default:
                 trace("something went wrong with the type");
         }
